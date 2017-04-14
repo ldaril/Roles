9ee30f8c39a9b0548a0dadf7fad09478e8e3501b
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 13:27:48 +0200

    permissions in config

---
+++ src/config/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'roles' => [
+        'index-roles' => 'Show list',
+        'create-role' => 'Create',
+        'store-role' => 'Store',
+        'edit-role' => 'Edit',
+        'update-role' => 'Update',
+        'destroy-role' => 'Destroy',
+    ],
+];
