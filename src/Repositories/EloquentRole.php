8f2a6dd9da40f3c4491fb8faba40c43366a6f022
Samuel De Backer <sdebacker@gmail.com>
2017-05-22 11:52:55 +0200

    unused method

--- src/Repositories/EloquentRole.php
+++ src/Repositories/EloquentRole.php
@@ -10,17 +10,4 @@ class EloquentRole extends EloquentRepository
     protected $repositoryId = 'roles';
 
     protected $model = Role::class;
-
-    /**
-     * Get all models.
-     *
-     * @param array $with Eager load related models
-     * @param bool  $all  Show published or all
-     *
-     * @return Collection|NestedCollection
-     */
-    public function all(array $with = [], $all = false)
-    {
-        return $this->with($with)->order()->get();
-    }
 }
